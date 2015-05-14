52da8baf99fc5966b0e50fd08ed85921e88247bf
dwl <dangwenliang@gmail.com>
2015-05-14 16:56:19 +0800

    Change activity to ActionBarActivity

--- ActivityTest/app/src/main/java/com/greatcl/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/greatcl/activitytest/FirstActivity.java
@@ -1,13 +1,12 @@
 package com.greatcl.activitytest;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
+import android.support.v7.app.ActionBarActivity;
 
 /**
  * Created by caolei on 15/5/14.
  */
-public class FirstActivity extends Activity {
+public class FirstActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceStace){
         super.onCreate(savedInstanceStace);
